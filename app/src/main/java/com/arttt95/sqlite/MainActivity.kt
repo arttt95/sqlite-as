f11d63ae50986f18ef7a2a25b5dd46c6e4188b4d
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-25 16:26:14 -0300

    25DEZ24 - Aula 197 AS - DatabaseHelper (onCreate & onUpgrade).

--- app/src/main/java/com/arttt95/sqlite/MainActivity.kt
+++ app/src/main/java/com/arttt95/sqlite/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.arttt95.sqlite
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arttt95.sqlite.database.DatabaseHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private val bancoDados by lazy {
+        DatabaseHelper(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val sql = "INSERT INTO " +
+                        "produtos " +
+                    "VALUES(" +
+                        "NULL, " +
+                        "'Notebook', " +
+                        "'Descrição...'" +
+                    ");"
+
+        try {
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("info_db", "Sucesso ao inserir registro main")
+        } catch (e: Exception) {
+            Log.i("info_db", "Erro ao inserir registro main")
         }
     }
 }
--- app/src/main/java/com/arttt95/sqlite/database/DatabaseHelper.kt
+++ app/src/main/java/com/arttt95/sqlite/database/DatabaseHelper.kt
@@ -3,26 +3,36 @@ package com.arttt95.sqlite.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(
     context,
     "loja.db",
     null,
-    1
+    2
 ){
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(
-            "CREATE TABLE " +
+
+        Log.i("info_db", "Executou onCreate -> Helper")
+
+        val sql = "CREATE TABLE IF NOT EXISTS " +
                     "produtos(" +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        "nome VARCHAR(100)," +
+                        "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                        "titulo VARCHAR(100), " +
                         "descricao TEXT" +
                     ");"
-        )
+
+        try {
+            db?.execSQL(sql)
+            Log.i("info_db", "Sucesso ao criar a table -> onCreate -> Helper")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i("info_db", "Erro ao criar a table -> onCreate ->Helper")
+        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        Log.i("info_db", "Executou onUpgrade -> Helper")
     }
 
 }
